a1b127c5001a54908c33029763c48a70ce9f66a4
lesinegor2001 <gon4arow.andre2016@yandex.ru>
2024-03-28 21:58:29 +0300

    mapping for GameDto

--- RollerCoaster/Services/GameService.cs
+++ RollerCoaster/Services/GameService.cs
@@ -1,4 +1,5 @@
 ﻿using RollerCoaster.DataBase;
+using RollerCoaster.DataBase.Models.Game;
 using RollerCoaster.DataTransferObjects.Game;
 
 namespace RollerCoaster.Services;
@@ -11,8 +12,51 @@ public class GameService (DataBaseContext dataBaseContext) : IGameService
         
         if (game is null)
             return null;
-        throw new NotImplementedException();
-        
+
+        return new GameDTO()
+        {
+            Name = game.Name,
+            Description = game.Description,
+            Locations = game.Locations.Select(l => new LocationDTO()
+            {
+                Name = l.Name,
+                Description = l.Description,
+                Id = "abc"
+                
+            }).ToList(),
+            Items = game.Items.Select(i => new  ItemDTO(){
+                Name = game.Name,
+                Description = i.Description,
+                ItemType = "iii"
+            
+            }).ToList(),
+            Quests = game.Quests.Select(quest => new QuestDTO()
+            {
+                Name = game.Name,
+                Description = quest.Description,
+                HiddenDescription = "hhh"
+            }).ToList(),
+            Skills = game.Skills.Select(skill => new SkillDTO()
+            {
+                Name = game.Name,
+                Description = skill.Description,
+                ForClassId = "fff"
+            }).ToList(),
+            NonPlayableCharacters = game.NonPlayableCharacters.Select(npc => new NonPlayableCharacterDTO()
+            {
+                Name = game.Name,
+                BaseLocationId = "bbb"
+            }).ToList(),
+            Classes = game.Classes.Select(cls => new CharacterClassDTO()
+            {
+                Name = game.Name,
+                Description = cls.Description,
+                Id = "iii"
+            }).ToList(),
+           
+                
+        };
+
     }
 
     public Task<int> Create(GameDTO game)
