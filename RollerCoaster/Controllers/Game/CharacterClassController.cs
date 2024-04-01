3e0838e97ebc1fd8aa6c7d313280f669454cadfe
lesinegor2001 <gon4arow.andre2016@yandex.ru>
2024-04-01 20:40:30 +0300

    controllers implementation

--- RollerCoaster/Controllers/Game/CharacterClassController.cs
+++ RollerCoaster/Controllers/Game/CharacterClassController.cs
@@ -1,26 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
+using RollerCoaster.DataTransferObjects.Game.Creation;
+using RollerCoaster.DataTransferObjects.Game.Fetching;
+using RollerCoaster.Services.Abstractions.Common;
+using RollerCoaster.Services.Abstractions.Game;
 
 namespace RollerCoaster.Controllers.Game;
 
 [Route("[controller]")]
 [ApiController]
-public class CharacterClassController(): ControllerBase
+public class CharacterClassController(ICharacterClassService characterClassService): ControllerBase
 {
     [HttpPost]
-    public async Task<IActionResult> Create()
+    public async Task<IActionResult> Create(CharacterClassCreationDTO characterClassCreationDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var userId = HttpContext.User.Claims.First(c => c.Type == "id").Value;
+            var createdNpcId = await characterClassService.Create(int.Parse(userId), characterClassCreationDto);
+            return Created("", new
+            {
+                Id = createdNpcId
+            });
+        }
+        catch (NotFoundError e)
+        {
+            return NotFound(new {e.Message});
+        }
+        catch (AccessDeniedError e)
+        {
+            return Forbid(e.Message);
+        }
     }
     
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var userId = HttpContext.User.Claims.First(c => c.Type == "id").Value;
+            await characterClassService.Delete(int.Parse(userId), id);
+            return Ok();
+        }
+        catch (NotFoundError e)
+        {
+            return NotFound(new {e.Message});
+        }
+        catch (AccessDeniedError e)
+        {
+            return Forbid(e.Message);
+        }
     }
     
     [HttpGet("{id:int}")]
     public async Task<IActionResult> Get(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var locationDto = await characterClassService.Get(id);
+            return Ok(locationDto);
+        }
+        catch (NotFoundError e)
+        {
+            return NotFound(new { e.Message });
+        }
     }
 }
